e49fc3f82e8412e7ddb88db67db114e5ddea8bfc
Rudy Rico <rjrico@yahoo.com>
2024-02-26 11:26:49 -0800

    explain the method

--- some.py
+++ some.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 def some(coll, pred=lambda x:x):
-    """Return true if pred(item) is true for some item in coll"""
+    """Return true if pred(item) is true for some item in coll
+    
+    The generator produces either 1+ True entries or no entries - which defaults to a False
+    The iterator returns the 1st item - either the 1st True or the default False
+    """
     return next((True for item in coll if pred(item)), False)
 
 def odd(n):
     return bool(n % 2)
 
 def main():
+    assert(some([]) == False)
+    assert(some([0]) == False)
+    assert(some([1]))
+    assert(some([0, ""]) == False)
     assert(some([0, "", True]))
     assert(some([0, "", False]) == False)
     assert(some([0, "", 4]))
